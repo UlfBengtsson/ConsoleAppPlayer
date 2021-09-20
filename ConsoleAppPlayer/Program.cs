14fc1152469cc448139eafca7e463b1d74926422
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-09-20 13:52:19 +0200

    name fix

--- ConsoleAppPlayer/Program.cs
+++ ConsoleAppPlayer/Program.cs
@@ -8,8 +8,8 @@ namespace ConsoleAppPlayer
         static void Main(string[] args)
         {
             Player player1 = new Player("Calle", "Human", "Man");
-            Player player2 = new Player("Calle", "Human", "Man");
-            Player player3 = new Player("Calle", "Human", "Man");
+            Player player2 = new Player("Adam", "Human", "Man");
+            Player player3 = new Player("Oskar", "Human", "Man");
             
             Console.WriteLine(player1.Status());
             Console.WriteLine(player2.Status());
