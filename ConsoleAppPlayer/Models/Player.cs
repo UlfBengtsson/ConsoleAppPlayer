48669b1e4bb63df8d6aa44a0473405f8d40d2846
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2021-09-20 13:45:39 +0200

    Add project files.

---
+++ ConsoleAppPlayer/Models/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppPlayer.Models
+{
+    public class Player
+    {
+        static Random random = new Random();
+        static string[] startCities = { "Växjö", "Stockholm", "Malmö", "Jönköping" };
+        //array index start at 0.       // i0    // i1        // i2    // i3
+
+        int vitalety;
+        int inteligance;
+        int strength;
+
+        string name;
+        string race;
+        string gender;
+
+        string borinIn;
+
+        public Player(string name, string race, string gender)
+        {
+            this.name = name;
+            this.race = race;
+            this.gender = gender;
+
+            vitalety = random.Next(1, 7);
+            inteligance = random.Next(1, 7);
+            strength = random.Next(1, 7);
+
+            borinIn = startCities[random.Next(startCities.Length)];
+            //Array.Fill(startCities, "_");
+            //Array.Resize();
+        }
+
+        public string Status()
+        {
+            return $"--- Status ---\nName: {name}\nRace: {race}\nGender: {gender}\nVit: {vitalety}\nStr: {strength}\nInt: {inteligance}\nBorn in: {borinIn}\n-----\n";
+        }
+    }
+}
---
+++ ConsoleAppPlayer/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppPlayer.Models;
+using System;
+
+namespace ConsoleAppPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player1 = new Player("Calle", "Human", "Man");
+            Player player2 = new Player("Calle", "Human", "Man");
+            Player player3 = new Player("Calle", "Human", "Man");
+            
+            Console.WriteLine(player1.Status());
+            Console.WriteLine(player2.Status());
+            Console.WriteLine(player3.Status());
+        }
+    }
+}
